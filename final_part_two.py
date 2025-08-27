0f7b385398f769edb5ba23c9433634fe3c887c70
Lukas Fenkam <lukas@fenkam.name>
2025-08-27 02:32:33 -0400

    Testing

--- final_part_two.py
+++ final_part_two.py
@@ -100,10 +100,10 @@ reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=4, min_lr
 # -------------------------------
 history = model.fit(
     train_generator,
-    epochs=300,
+    epochs=100,
     validation_data=val_generator,
     class_weight=class_weights_dict,
-    callbacks=[early_stop, reduce_lr]
+    callbacks=[reduce_lr]
 )
 
 # -------------------------------
